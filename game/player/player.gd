4730eaf091295f277c1882642ad061a40ebe8cca
5hafin <Sky-Shift@yandex.ru>
2024-05-09 16:00:07 +0700

    система диалогов

--- game/player/player.gd
+++ game/player/player.gd
@@ -6,16 +6,20 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var select_dialogue = false
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Add the gravity.
+	
+		
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * _delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,3 +30,32 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if select_dialogue == true:
+		if Input.is_action_just_pressed("ui_select"):
+			DialogueManager.show_example_dialogue_balloon(load("res://dialogues/dialoguestart.dialogue"), "start")
+			return
+
+
+func _on_detection_zone_area_entered(_body):
+	print("вошёл")
+	if _body.has_method("dialogue"):
+		select_dialogue = true
+		print(select_dialogue)
+		
+	
+	pass # Replace with function body.
+
+
+
+
+func _on_detection_zone_body_entered(_body):
+	print("вошёл")
+	if _body.has_method("dialogue"):
+		select_dialogue = true
+		print(select_dialogue)
+	pass # Replace with function body.
+
+
+func _on_detection_zone_body_exited(_body):
+	pass # Replace with function body.
